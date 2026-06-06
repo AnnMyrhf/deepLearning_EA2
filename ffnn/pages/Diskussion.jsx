37c2a5b839b1a13507ecac6dc275ca61a90789ab
AnnMyrhf <ann-kathrin.meyerhof@stud.th-luebeck.de>
2026-06-06 12:24:17 +0200

    Zwinge Git die Ordnerstruktur von ffnn korrekt zu übernehmen

---
+++ ffnn/pages/Diskussion.jsx
@@ -0,0 +1,19 @@
+export default function Diskussion() {
+    return (
+        <div className="container py-5 mb-5 pb-5">
+            <div className="row justify-content-center">
+                {/* Haupt-Header */}
+                <h1 className="display-4 fw-bold text-light mb-4">Diskussion</h1>
+                <div className="mb-5">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+                </div>
+            </div>
+        </div>
+    )
+}
